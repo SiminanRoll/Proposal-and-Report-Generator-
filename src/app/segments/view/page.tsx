8e5da168c3c8f62d5f6a2b4dd1789afe93013e42
PattyPoo <patricb@live.com>
2026-08-07 18:06:07 -0500

    Add static segment detail route

---
+++ src/app/segments/view/page.tsx
@@ -0,0 +1,6 @@
+import { AppShell } from "@/components/app-shell";
+import { SegmentDetailPage } from "@/components/segment-detail-page";
+
+export default function SegmentViewPage() {
+  return <AppShell><SegmentDetailPage /></AppShell>;
+}
