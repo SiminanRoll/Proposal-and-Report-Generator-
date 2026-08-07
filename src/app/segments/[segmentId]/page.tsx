01ec7c8899a474e9c435d44411dc2c63ad9c44de
PattyPoo <patricb@live.com>
2026-08-07 18:06:19 -0500

    Remove dynamic segment route incompatible with static export

--- src/app/segments/[segmentId]/page.tsx
+++
@@ -1,6 +0,0 @@
-import { AppShell } from "@/components/app-shell";
-import { SegmentDetailPage } from "@/components/segment-detail-page";
-
-export default function SegmentPage() {
-  return <AppShell><SegmentDetailPage /></AppShell>;
-}
