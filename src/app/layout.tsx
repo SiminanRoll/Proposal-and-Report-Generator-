d6dc6465c988c699e2e1cf1bb36440749692fbdc
PattyPoo <patricb@live.com>
2026-08-08 09:22:55 -0500

    Load v1.0.9.33 map hotfix styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,7 @@ import "./v10929-polish.css";
 import "./v10930-polish.css";
 import "./v10931-polish.css";
 import "./v10932-map-interactions.css";
+import "./v10933-map-hotfix.css";
 
 export const metadata: Metadata = {
   title: "Client Compass",
