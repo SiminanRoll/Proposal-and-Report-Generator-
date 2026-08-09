c99a3f9bd05bd5d657bacf669e8c385667c0f828
PattyPoo <patricb@live.com>
2026-08-09 13:40:54 -0500

    Pause unfinished review cloud rollout during data reconciliation

--- src/components/client-compass-runtime.tsx
+++ src/components/client-compass-runtime.tsx
@@ -2,13 +2,12 @@
 
 import { CaptainsLogCrossDeviceRuntime } from "./captains-log-cross-device-runtime";
 import { ClientActivityRuntime } from "./client-activity-runtime";
-import { ClientReviewCloudRuntime } from "./client-review-cloud-runtime";
 import { ClientWorkspacePolishRuntime } from "./client-workspace-polish-runtime";
 import { InterfacePolishRuntimeV10939 } from "./interface-polish-runtime-v10939";
 import { MapCompassRuntimeV10934 } from "./map-compass-runtime-v10934";
 import { MapDisplayRuntime } from "./map-display-runtime";
 import { MapHardwareSyncStampV10946 } from "./map-hardware-sync-stamp-v10946";
-import { MapInteractionPolishV10932 } from "./map-interaction-polish-v10932";
+import { MapInteractionPolishV10932 } from "./map-interaction-polish-runtime-v10932";
 import { MapModeControllerV10945 } from "./map-mode-controller-v10945";
 import { MapSegmentDrawerV10931 } from "./map-segment-drawer-v10931";
 import { MapSelectionGroupBridge } from "./map-selection-group-bridge";
@@ -19,7 +18,6 @@ import { WorkbenchRuntime } from "./workbench-runtime";
 export function ClientCompassRuntime() {
   return <>
     <CaptainsLogCrossDeviceRuntime />
-    <ClientReviewCloudRuntime />
     <MapSelectionGroupBridge />
     <MapSegmentDrawerV10931 />
     <MapInteractionPolishV10932 />
