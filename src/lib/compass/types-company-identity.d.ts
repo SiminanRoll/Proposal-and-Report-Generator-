3e7bea87b7a5f445fa9bafe9082e382b4d97060a
PattyPoo <patricb@live.com>
2026-08-09 14:09:32 -0500

    Augment Compass records with universal company IDs

---
+++ src/lib/compass/types-company-identity.d.ts
@@ -0,0 +1,19 @@
+import "./types";
+
+declare module "./types" {
+  interface CompassCaptainsLogTask {
+    companyId?: string;
+  }
+
+  interface CompassCaptainsLogActivity {
+    companyId?: string;
+  }
+
+  interface CompassCaptainsLogState {
+    companyId?: string;
+  }
+
+  interface CompassClient {
+    companyId?: string;
+  }
+}
