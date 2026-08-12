7871d0fc488fa630ff69efe04a20e96518719e03
PattyPoo <patricb@live.com>
2026-08-11 22:30:29 -0500

    Mount map stats integrity runtime

--- src/components/client-compass-runtime.tsx
+++ src/components/client-compass-runtime.tsx
@@ -13,6 +13,7 @@ import { MapInteractionPolishV10932 } from "./map-interaction-polish-v10932";
 import { MapModeControllerV10945 } from "./map-mode-controller-v10945";
 import { MapSegmentDrawerV10931 } from "./map-segment-drawer-v10931";
 import { MapSelectionGroupBridge } from "./map-selection-group-bridge";
+import { MapStatsIntegrityRuntime } from "./map-stats-integrity-runtime";
 import { MapUiRuntime } from "./map-ui-runtime";
 import { ReportCompanyDetailsBridge } from "./report-company-details-bridge";
 import { WorkbenchRuntime } from "./workbench-runtime";
@@ -26,6 +27,7 @@ export function ClientCompassRuntime() {
     <MapInteractionPolishV10932 />
     <MapModeControllerV10945 />
     <MapCompassRuntimeV10934 />
+    <MapStatsIntegrityRuntime />
     <MapDisplayRuntime />
     <MapHardwareSyncStampV10946 />
     <MapUiRuntime />
