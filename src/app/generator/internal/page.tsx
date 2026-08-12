67be28722b58d9f44fe86092599a45b1db2cce50
PattyPoo <patricb@live.com>
2026-08-12 16:13:55 -0500

    Add internal TC report generator route

---
+++ src/app/generator/internal/page.tsx
@@ -0,0 +1,10 @@
+import { AppShell } from "@/components/app-shell";
+import { InternalTcReportGenerator } from "@/components/internal-tc-report-generator";
+
+export default function InternalTcReportPage() {
+  return (
+    <AppShell>
+      <InternalTcReportGenerator />
+    </AppShell>
+  );
+}
