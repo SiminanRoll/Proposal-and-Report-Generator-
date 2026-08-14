191d4eecf37291c997d52e9713a5d25cb92ad8a5
PattyPoo <patricb@live.com>
2026-08-14 16:43:30 -0500

    Add recovery settings route

---
+++ src/app/settings/recovery/page.tsx
@@ -0,0 +1,6 @@
+import { AppShell } from "@/components/app-shell";
+import { CompassRecoverySettingsPage } from "@/components/compass-recovery-settings-page";
+
+export default function RecoverySettingsPage() {
+  return <AppShell><CompassRecoverySettingsPage /></AppShell>;
+}
