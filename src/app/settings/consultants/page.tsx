41008a70878b8dc0e0acee8ca3e7d03428cc54a8
PattyPoo <patricb@live.com>
2026-08-14 16:42:54 -0500

    Add consultant settings route

---
+++ src/app/settings/consultants/page.tsx
@@ -0,0 +1,6 @@
+import { AppShell } from "@/components/app-shell";
+import { CompassConsultantSettingsPage } from "@/components/compass-consultant-settings-page";
+
+export default function ConsultantSettingsPage() {
+  return <AppShell><CompassConsultantSettingsPage /></AppShell>;
+}
