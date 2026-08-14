7ca16a93541da44c01eb637d58397c90cff09aa1
PattyPoo <patricb@live.com>
2026-08-14 13:59:01 -0500

    Add pricing settings route

---
+++ src/app/settings/pricing/page.tsx
@@ -0,0 +1,6 @@
+import { AppShell } from "@/components/app-shell";
+import { CompassPricingSettingsPage } from "@/components/compass-pricing-settings-page";
+
+export default function PricingSettingsPage() {
+  return <AppShell><CompassPricingSettingsPage /></AppShell>;
+}
